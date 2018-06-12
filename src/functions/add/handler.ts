6d38516344bdb13be775ab9b190ccf68fc0179e7
Jaro Klc <github@jaroklc.net>
2018-06-12 10:19:38 -0400

    fixed testing problems

--- src/functions/add/handler.ts
+++ src/functions/add/handler.ts
@@ -15,8 +15,8 @@ export function add(event: any, context: any, callback: any): void {
   if (typeof request.a === "undefined" || typeof request.b === "undefined")
     return callback(null, createResponseObject(400, { "message": "[400] Error!  arguments a and b are required!" }));
 
-  if (typeof request.a === "number" || typeof request.b === "number")
-    return callback(null, createResponseObject(400, { message: "[400] Error!  arguments a and b must be strings!" }));
+  if (typeof request.a !== "number" || typeof request.b !== "number")
+    return callback(null, createResponseObject(400, { message: "[400] Error!  arguments a and b must be numbers!" }));
 
   const response = {
     message: "Go Serverless v1.0! Your add function executed successfully!",
